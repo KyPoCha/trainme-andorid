0c8d107da42928386242a5488dd9876c964617bd
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2023-05-29 12:27:09 +0200

    KP | added injection hilt resolving

---
+++ app/src/main/java/cz/cvut/fit/poliskyr/trainmeapp/App.kt
@@ -0,0 +1,7 @@
+package cz.cvut.fit.poliskyr.trainmeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
--- app/src/main/java/cz/cvut/fit/poliskyr/trainmeapp/MainActivity.kt
+++ app/src/main/java/cz/cvut/fit/poliskyr/trainmeapp/MainActivity.kt
@@ -6,37 +6,43 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import cz.cvut.fit.poliskyr.trainmeapp.data.db.TrainersRepository
+import cz.cvut.fit.poliskyr.trainmeapp.navigation.Navigation
+import cz.cvut.fit.poliskyr.trainmeapp.presentation.AuthViewModel
+import cz.cvut.fit.poliskyr.trainmeapp.presentation.MainViewModel
+import cz.cvut.fit.poliskyr.trainmeapp.presentation.TrainersViewModel
+import cz.cvut.fit.poliskyr.trainmeapp.presentation.TrainingsViewModel
 import cz.cvut.fit.poliskyr.trainmeapp.ui.theme.TrainMeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject lateinit var trainersRepository: TrainersRepository
+    private val trainersViewModel = TrainersViewModel()
+    private val trainingsViewModel = TrainingsViewModel()
+    private val mainViewModel = MainViewModel()
+    private val authViewModel = AuthViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        trainersViewModel.trainersRepository = trainersRepository
+        trainingsViewModel.trainersRepository = trainersRepository
         setContent {
+            setTheme(R.style.Theme_TrainMeApp)
             TrainMeAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Navigation(
+                        trainersViewModel = trainersViewModel,
+                        trainingsViewModel = trainingsViewModel,
+                        mainViewModel = mainViewModel,
+                        authViewModel = authViewModel
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TrainMeAppTheme {
-        Greeting("Android")
     }
 }
